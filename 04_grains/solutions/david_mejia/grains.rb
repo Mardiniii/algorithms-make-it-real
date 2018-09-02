97e6ec75f9c96bd2196fd61bd776e53787c862ff
David Mejia <davidndy@gmail.com>
2018-09-02 12:00:42 -0500

    Add max squares as class constant

--- 04_grains/solutions/david_mejia/grains.rb
+++ 04_grains/solutions/david_mejia/grains.rb
@@ -1,8 +1,10 @@
 class Grains 
+  MAX_SQUARES = 64
+
   def self.chessboard_grains
     @chessboard = [1]
 
-    (64-1).times { |i| @chessboard << @chessboard[-1]*2  }
+    (MAX_SQUARES-1).times { |i| @chessboard << @chessboard[-1]*2  }
   end
 
   def self.total
@@ -10,7 +12,7 @@ class Grains
   end
 
   def self.square(x)
-    raise ArgumentError if x < 1 || x > 64
+    raise ArgumentError if x < 1 || x > MAX_SQUARES
 
     chessboard_grains unless @chessboard
 
@@ -18,9 +20,4 @@ class Grains
   end
 end
 
-p Grains.square(16)
-p Grains.square(32)
-p Grains.square(8)
-p Grains.square(24)
-p Grains.square(44)
 
